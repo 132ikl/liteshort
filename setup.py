1b7ca2d517ec3f14bdc2e72247ead163bbbcc413
132ikl <132@ikl.sh>
2020-04-10 05:03:41 -0400

    Downgrade setup.py to current, remove CONVENTIONS, update config

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="liteshort",
-    version="2.0.0",
+    version="1.1.0",
     author="Steven Spangler",
     author_email="132@ikl.sh",
     description="User-friendly, actually lightweight, and configurable URL shortener",
